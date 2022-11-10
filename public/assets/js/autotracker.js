4f432382a45a0be405dcf4477bd04c1e526152c3
Taiwo <ttejuosho@outlook.com>
2022-11-09 21:39:55 -0600

    js files fix

--- public/assets/js/autotracker.js
+++ public/assets/js/autotracker.js
@@ -22,36 +22,6 @@ $("#closeNewTrackerModal").on("click", () => {
   $("#newTrackerModal").modal("hide");
 });
 
-$("#expiresOn").datetimepicker({
-  format: "L",
-});
-
-$("#registeredOn").datetimepicker({
-  format: "L",
-});
-
-$("#lastLoginDate").datetimepicker({
-  format: "L",
-});
-
-$("#expiresOn").on("change.datetimepicker", function (e) {
-  $("#expiresOnValue").val(
-    $("#expiresOn").data("datetimepicker").date().format("MM/DD/YYYY")
-  );
-});
-
-$("#registeredOn").on("change.datetimepicker", function (e) {
-  $("#registeredOnValue").val(
-    $("#registeredOn").data("datetimepicker").date().format("MM/DD/YYYY")
-  );
-});
-
-$("#lastLoginDate").on("change.datetimepicker", function (e) {
-  $("#lastLoginDateValue").val(
-    $("#lastLoginDate").data("datetimepicker").date().format("MM/DD/YYYY")
-  );
-});
-
 function renderAutoTrackerTable(data) {
   autoTrackerTable = $("#autoTrackerTable").DataTable({
     data: data,
